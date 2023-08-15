5076821ee2c5d0a30b1b1b326ef6a0a6fc9c8e34
jtrenaud1s <jtrenaud1s@semo.edu>
2023-08-14 23:54:42 -0500

    left offcanvas  support

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -12,16 +12,7 @@ const Layout = ({ children }: LayoutProps): JSX.Element => {
     <>
       <div style={{ overflow: "hidden" }}>
         <div>{children}</div>
-        <button
-          className="absolute left-0 top-10 bg-green-600 text-white rounded px-4 py-1 z-450"
-          onClick={() => setIsOpen(true)}>
-          Toggle
-        </button>
-        <Offcanvas
-          title="test"
-          isOpen={isOpen}
-          setIsOpen={setIsOpen}
-          side="right">
+        <Offcanvas isOpen={isOpen} setIsOpen={setIsOpen} position="left">
           <TabsComponent />
         </Offcanvas>
       </div>
--- src/components/Offcanvas.tsx
+++ src/components/Offcanvas.tsx
@@ -1,39 +1,97 @@
-import { ReactNode } from "react";
+import React, { ReactNode, useRef, useState, useEffect } from "react";
 
 interface OffcanvasProps {
   children: ReactNode;
   isOpen: boolean;
   setIsOpen: (isOpen: boolean) => void;
+  position?: "left" | "right";
 }
 
 const Offcanvas = ({
   children,
   isOpen,
   setIsOpen,
+  position = "right",
 }: OffcanvasProps): JSX.Element => {
+  const offcanvasRef = useRef<HTMLDivElement>(null);
+  const [offcanvasWidth, setOffcanvasWidth] = useState(0);
+
+  useEffect(() => {
+    if (offcanvasRef.current) {
+      setOffcanvasWidth(offcanvasRef.current.offsetWidth);
+    }
+  }, [isOpen]);
+
+  const toggleButtonBaseClasses =
+    "fixed z-450 bg-green-600 text-white rounded px-4 py-1 transition-transform duration-500 ease-in-out";
+
+  const toggleButtonPositionClasses = {
+    right: "top-1/2 translate-x-1/2 right-0",
+    left: "top-1/2 translate-x-1/2 left-0",
+  };
+
+  const toggleButtonClasses = `${toggleButtonBaseClasses} ${toggleButtonPositionClasses[position]}`;
+
+  const backdropClasses =
+    "fixed overflow-hidden bg-gray-900 bg-opacity-25 inset-0 transform ease-in-out z-450";
+
+  const offcanvasClasses = {
+    left: "w-screen max-w-lg left-0 absolute bg-white h-full shadow-xl delay-400 duration-500 ease-in-out transition-all transform",
+    right:
+      "w-screen max-w-lg right-0 absolute bg-white h-full shadow-xl delay-400 duration-500 ease-in-out transition-all transform",
+  };
+  const toggleButtonStyle = {
+    left: {
+      transform: isOpen ? `translateX(${offcanvasWidth}px)` : "translateX(0)",
+    },
+    right: {
+      transform: isOpen ? `translateX(-${offcanvasWidth}px)` : "translateX(0)",
+    },
+  };
+
+  const offcanvasStyle = {
+    right: {
+      transform: isOpen ? "translateX(0)" : "translateX(100%)",
+    },
+    left: {
+      transform: isOpen ? "translateX(0)" : "translateX(-100%)",
+    },
+  };
+
+  const backdropTransitions = {
+    right: isOpen
+      ? " transition-opacity opacity-100 duration-500 translate-x-0"
+      : " transition-all duration-500 opacity-0 translate-x-full",
+    left: isOpen
+      ? " transition-opacity opacity-100 duration-500 translate-x-0"
+      : " transition-all  duration-500 opacity-0 -translate-x-full",
+  };
+
   return (
-    <main
-      className={
-        " fixed overflow-hidden bg-gray-900 bg-opacity-25 inset-0 transform ease-in-out z-450" +
-        (isOpen
-          ? " transition-opacity opacity-100 duration-500 translate-x-0  "
-          : " transition-all delay-500 opacity-0 translate-x-full  ")
-      }>
-      <section
-        className={
-          " w-screen max-w-lg right-0 absolute bg-white h-full shadow-xl delay-400 duration-500 ease-in-out transition-all transform  " +
-          (isOpen ? " translate-x-0 " : " translate-x-full ")
-        }>
-        <article className="relative w-screen max-w-lg pb-10 flex flex-col space-y-6 overflow-y-scroll h-full">
+    <>
+      {/* Toggle Button */}
+      <button
+        style={{ ...toggleButtonStyle[position] }}
+        className={toggleButtonClasses}
+        onClick={() => setIsOpen(!isOpen)}>
+        Toggle
+      </button>
+      {/* Backdrop */}
+      <main className={backdropClasses + backdropTransitions[position]}>
+        {/* Offcanvas */}
+        <section
+          ref={offcanvasRef}
+          style={{ ...offcanvasStyle[position] }}
+          className={offcanvasClasses[position]}>
           {children}
-        </article>
-      </section>
-      <section
-        className=" w-screen h-full cursor-pointer "
-        onClick={() => {
-          setIsOpen(false);
-        }}></section>
-    </main>
+        </section>
+        <section
+          className="w-screen h-full cursor-pointer"
+          onClick={() => {
+            setIsOpen(false);
+          }}></section>
+      </main>
+    </>
   );
 };
 
